ba4f2e12f215eac08e22e46f920faba8b366d0e0
jracek <racek.janek@gmail.com>
2024-01-06 23:04:19 +0100

    Made the texture do a little DVD screensaver bounce lol

--- game/main.cpp
+++ game/main.cpp
@@ -8,7 +8,6 @@
 
 #include "game/game.h"
 
-
 Scene* scene1;
 GameObject* gameObject1;
 Sprite* sprite1;
@@ -16,14 +15,21 @@ Texture* texture1;
 
 extern const uint8_t textureData[];
 
-void game_setup() {
+// Define DVD screensaver speed
+int SPEED_X = 2;
+int SPEED_Y = 1;
 
+void game_setup() {
     scene1 = new Scene();
-    gameObject1 = new GameObject(100,100,100);
+    gameObject1 = new GameObject(100, 100, 100);
     sprite1 = new Sprite(SPRITE_TYPE_TEXTURED);
     texture1 = new Texture();
+    
 
-    psbw_upload_texture(texture1, textureData, SCREEN_WIDTH*2, 0, 64, 64);
+    psbw_upload_texture(texture1, textureData, SCREEN_WIDTH * 2, 0, 64, 64);
+
+    sprite1->Width = texture1->width;
+    sprite1->Height = texture1->height;
 
     sprite1->tex = texture1;
 
@@ -33,12 +39,21 @@ void game_setup() {
     gameObject1->position.x = 0;
     gameObject1->position.y = 0;
 
-    sprite1->Width = 40;
-    sprite1->Height = 40;
-
     psbw_load_scene(scene1);
 }
 
 void game_loop() {
-    sio_print("test");
+    // Move the game object
+    gameObject1->position.x += SPEED_X;
+    gameObject1->position.y += SPEED_Y;
+
+    // Bounce off the walls
+    if (gameObject1->position.x <= 0 || gameObject1->position.x + sprite1->Width >= SCREEN_WIDTH) {
+        SPEED_X = -SPEED_X;
+    }
+
+    if (gameObject1->position.y <= 0 || gameObject1->position.y + sprite1->Height >= SCREEN_HEIGHT) {
+        SPEED_Y = -SPEED_Y;
+    }
+
 }
